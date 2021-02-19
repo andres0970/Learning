04c69a9578957afe4338c266c05b1d96865568dc
ANDRES <andresobendorfer@hotmail.com>
2021-02-19 08:01:02 -0500

    Functional test

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-def fibonnaci (n):
-    a= 0
-    b= 1
-    for i in range (1,n):
-        c= a +b
-        a=b
-        b=c
-        print (b)
+def fibonnaci(number):
+    if number == 0:
+        print (1)
+    else:
+        a = 0
+        b = 1
+        for i in range(1, number):
+            c = a + b
+            a = b
+            b = c
+        return b
 
-fibonnaci(5)
+
+def user_inter ():
+    num = int(input("Cuantos numeros de la serie "))
+    result = fibonnaci(num)
+    print(result)
+
+def test_fibonnaci():
+    assert fibonnaci(9) == 34
+    assert fibonnaci(1) == 1
+    assert fibonnaci(2) == 1
+
+
+if __name__ == '__main__':
+    test_fibonnaci()
